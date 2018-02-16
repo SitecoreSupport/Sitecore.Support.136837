c510e6247123b578c7af64d37ffe4d52ca382b14
DK\ili <ili@sitecore.net>
2018-02-16 15:04:23 +0200

    Release

---
+++ src/Sitecore.Support.136837/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.136837")]
+[assembly: AssemblyProduct("Sitecore.Support.136837")]
+[assembly: ComVisible(false)]
